1542f85725cedf465e196d10a635498f751fc9fd
Murat Çay <68991695+MuratCay@users.noreply.github.com>
2025-02-19 19:45:54 +0300

    feat: Add Result and RequestHelper classes. - Added a `Result` sealed class to handle success, loading, and error states. - Added a `RequestHelper` to manage network or data requests. - Applied Hilt and kapt plugins to domain module build.gradle.kts file. - Added dependencies to domain module for hilt.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -33,6 +35,9 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
